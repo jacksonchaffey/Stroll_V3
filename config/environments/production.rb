bbc1c9b1a90ae85f777b1773855c6ef3bf7eed0f
jacksonchaffey <jacksonchaffey@gmail.com>
2020-11-27 13:47:21 +1100

    added mailer for production for pw resets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   
+  config.action_mailer.default_url_options = { :host => "https://morning-forest-87727.herokuapp.com" }
 end
